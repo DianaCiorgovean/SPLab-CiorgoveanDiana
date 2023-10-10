95121cb25688aaf53e847c34501ce8f09eb1c1e5
DianaCiorgovean <diana.ciorgovean02@e-uvt.ro>
2023-10-10 09:26:05 +0300

    changes

---
+++ src/test/java/ro/uvt/info/splabciorgoveandiana/SpLabCiorgoveanDianaApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splabciorgoveandiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLabCiorgoveanDianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
